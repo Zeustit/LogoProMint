1499dfb6c48d632d94857421fdfbf50218068877
YaroslavShl <98254571+YaroslavShl@users.noreply.github.com>
2022-02-14 20:26:36 +0300

    Update LogoProMint.sh

--- LogoProMint.sh
+++ LogoProMint.sh
@@ -1,5 +1,5 @@
-echo -e "\033[1;34m"
-echo -e "\033[1;36m"
+echo -e "\033[1;35m"
+echo -e "\033[1;35m"
 echo
 echo -e "/$$$$$$$                     /$$      /$$ /$$             /$$       " 
 echo -e "| $$__  $$                   | $$$    /$$$|__/            | $$      "
@@ -10,5 +10,5 @@ echo -e "| $$     | $$      | $$  | $$| $$\  $ | $$| $$| $$  | $$  | $$ /$$  "
 echo -e "| $$     | $$      |  $$$$$$/| $$ \/  | $$| $$| $$  | $$  |  $$$$/  "
 echo -e "|__/     |__/       \______/ |__/     |__/|__/|__/  |__/   \___/    "
 echo
-echo -e "\033[1;32mTelegram channel: \033[5;31mhttps://t.me/ProMintChannel\033[0m"
+echo -e "\033[1;33mTelegram channel: \033[5;31mhttps://t.me/ProMintChannel\033[0m"
 echo -e "\033[0m"
